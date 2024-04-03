a8515381aba71025961c37030b8e82f0751ce3d5
Nate Adams <nate.adams@umontana.edu>
2024-04-03 13:12:53 -0600

    Added comments and started assignment
    Added comments to both the README and to the assignment itself
    
    Added functions and names to update assignment for Lab submission

--- Go/GoBoard.java
+++ Go/GoBoard.java
@@ -1,43 +1,56 @@
 package Go;
-
 import java.util.Scanner;
 
 public class GoBoard {
+    static boolean[][] lives = new boolean[9][9];
+    static boolean[][] territory = new boolean[9][9];
+    static boolean[][] beenChecked = new boolean[9][9];
 
     public static void main(String[] args) {
-        String[][] Board = new String[10][10];
-        Scanner myObj = new Scanner(System.in);
-        Boolean cont = true;
+        String[][] board = new String[10][10];
+        Scanner scn = new Scanner(System.in);
+        boolean cont = true;
+        System.out.println("");
 
         System.out.println(" 0 1 2 3 4 5 6 7 8 9");
         while (cont) {
-            for (int i = 0; i < Board[0].length; i++) {
+            for (int i = 0; i < board.length; i++) {
                 System.out.print(i + " ");
-                for (int j = 0; j < Board.length; j++) {
-                    if (Board[i][j] == null) {
-                        if (j == 0) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j] == null) {
+                        if (j == 0 || j == 9) {
                             System.out.print("|");
                         } else {
-                            System.out.print("-|");
+                            System.out.print("＋");
                         }
-
                     } else {
-                        System.out.println();
+                        System.out.print(board[i][j]);
                     }
-
                 }
                 System.out.println();
             }
-
+                //add in board bounds here to fix shit
             System.out.println("Black Piece Turn: ◯ ");
             System.out.print("Please enter the x coordinate of your move:");
-            int moveX = myObj.nextInt();
+            int moveX = scn.nextInt();
             System.out.print("Please enter the y coordinate of your move:");
-            int moveY = myObj.nextInt();
+            int moveY = scn.nextInt();
+            board[moveX][moveY] = "◯ ";
+            //currently is line breaking the row, find fix?, using white piece for clarity
             System.out.println();
         }
+        scn.close();
+
     }
 }
-        // go pieces ◯ ●
-        // Board[9][9] = "◯";
-        // Board[9][8] = "●";
+//" ◯ ";
+//" ● ";
+
+//to do
+/*
+ * create board bounds
+ * capture peices (one or many)
+ * basic scoring piece
+ * (one of these 2 must recurse)
+ * 
+ */
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A common game of Go.
 
 This is for a class assignment for CSCI 152 for the Uniersity of Montana. In this program, there will be a game of go, with a board, capturing, and scoring. 
 
+Assignment requires scoring and capturing as well as using recursion for one of those 2 functions.
+
 How to play (From the British Go Association https://www.britgo.org/intro/intro2.html):
 
 A game of Go starts with an empty board. Each player has an effectively unlimited supply of pieces (called stones), one taking the black stones, the other taking white. The main object of the game is to use your stones to form territories by surrounding vacant areas of the board. It is also possible to capture your opponent's stones by completely surrounding them.
